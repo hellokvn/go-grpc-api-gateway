d7bd5d50c4fcaabfbd2549a7f6b97abb48852473
Kevin <hellokevinvogel@gmail.com>
2022-03-28 15:48:51 +0200

    chore(): add env config

---
+++ pkg/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port          string `mapstructure:"PORT"`
+	AuthSvcUrl    string `mapstructure:"AUTH_SVC_URL"`
+	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
+	OrderSvcUrl   string `mapstructure:"ORDER_SVC_URL"`
+}
+
+func LoadConfig() (config Config, err error) {
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+
+	return
+}
